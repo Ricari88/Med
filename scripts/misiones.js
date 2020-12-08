933ad100fb8b694d604d6a654d9881500f8459da
Ricardo <ricardo.cabrera.rivera@gmail.com>
2020-12-07 23:26:16 -0800

    button

--- scripts/misiones.js
+++ scripts/misiones.js
@@ -3,6 +3,6 @@ var misiones = document.querySelector("#misiones");
 var mision = ["uno", "dos", "tres", "cuatro"];
 
 mision.forEach(element => {
-    misiones.innerHTML += `<div class='mision' id='${element}'>${element.toUpperCase()}</div>`
+    misiones.innerHTML += `<button class='mision' id='${element}'>${element.toUpperCase()}</button>`
 });
 
