675c48e8b8e16616cc254ec241d02de0e801cf49
Ricardo <ricardo.cabrera.rivera@gmail.com>
2020-12-07 22:56:56 -0800

    array

--- scripts/misiones.js
+++ scripts/misiones.js
@@ -2,7 +2,7 @@
 
 var misiones = document.querySelector("#misiones");
 
-var mision = ["sistema solar", "mundial", "local", "personal"];
+var mision = ["uno", "dos", "tres", "cuatro"];
 
 mision.forEach(element => {
     misiones.innerHTML += `<div class='mision' id='${element}'>${element.toUpperCase()}</div>`
