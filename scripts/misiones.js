7b49860008d16a62177e6298e5cc675faac909db
Ricardo <ricardo.cabrera.rivera@gmail.com>
2020-12-08 08:15:02 -0800

    renew modal

---
+++ scripts/misiones.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var misiones = document.querySelector("#misiones");
+
+var mision = ["uno", "dos", "tres", "cuatro"];
+
+mision.forEach(element => {
+    misiones.innerHTML += `<button class='mision' id='${element}'>${element.toUpperCase()}</button>`
+});
+
