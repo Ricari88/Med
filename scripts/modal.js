9c76056ed91770f4449f39ef1f009c5e1033142b
Ricardo <ricardo.cabrera.rivera@gmail.com>
2020-12-07 23:06:07 -0800

    click modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -29,6 +29,17 @@ span.onclick = function() {
 });
 
 
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
 
 
 
